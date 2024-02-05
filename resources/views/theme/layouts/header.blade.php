7c3b7e7656c8454b0dab8a08fff791e68d26c653
thugtech97 <juhde.1997@gmail.com>
2024-02-05 12:50:02 +0800

    changes in seeders, storage links

--- resources/views/theme/layouts/header.blade.php
+++ resources/views/theme/layouts/header.blade.php
@@ -8,13 +8,13 @@
 				============================================= -->
 				<div id="logo">
 					<a href="{{ route('home') }}" class="standard-logo" data-dark-logo="{{ Setting::get_company_logo_storage_path() }}">
-						<img src="{{ asset(Setting::get_company_logo_storage_path()) }}" 
+						<img src="{{ asset('storage/logos/'.Setting::get_company_logo_storage_path()) }}" 
 							 alt="{{ Setting::info()->company_name }}" 
 							 onerror="handleBrokenImage(this, '{{ Setting::info()->company_name }}')">
 					</a>
 					
 					<a href="{{ route('home') }}" class="retina-logo" data-dark-logo="{{ Setting::get_company_logo_storage_path() }}">
-						<img src="{{ asset(Setting::get_company_logo_storage_path()) }}" 
+						<img src="{{ asset('storage/logos/'.Setting::get_company_logo_storage_path()) }}" 
 							 alt="{{ Setting::info()->company_name }}" 
 							 onerror="handleBrokenImage(this, '{{ Setting::info()->company_name }}')">
 					</a>
